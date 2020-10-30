0121a5b2f2f9dd41ded6492a685c04e4d6ca8b50
cmillecan <cmillecan@gmail.com>
2020-10-29 21:42:40 -0700

    include build since server is so slow

---
+++ client/build/precache-manifest.95cc6cf66c2b37688e1ee02876cbd764.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b498d527064d9e236560a21bf229fb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7e94b8bf475afa0ff66",
+    "url": "/static/css/2.44309f2b.chunk.css"
+  },
+  {
+    "revision": "806d51f71f1d1fae7c62",
+    "url": "/static/css/main.4c3a8c0b.chunk.css"
+  },
+  {
+    "revision": "f7e94b8bf475afa0ff66",
+    "url": "/static/js/2.78fe2de7.chunk.js"
+  },
+  {
+    "revision": "b303cf1a9e78c3d573683305f459fd10",
+    "url": "/static/js/2.78fe2de7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806d51f71f1d1fae7c62",
+    "url": "/static/js/main.31b272c8.chunk.js"
+  },
+  {
+    "revision": "99351373eac5a2708a1b",
+    "url": "/static/js/runtime-main.155dc714.js"
+  },
+  {
+    "revision": "72f600e4a70778ed5d2ea1f8d84dbfdc",
+    "url": "/static/media/KB.72f600e4.svg"
+  },
+  {
+    "revision": "da40728dc261bf181e40b83609636e11",
+    "url": "/static/media/about.da40728d.JPG"
+  },
+  {
+    "revision": "e2b09443a85ff7013bcc5b65a87e4098",
+    "url": "/static/media/plan.e2b09443.png"
+  },
+  {
+    "revision": "6e0a54d1e08f629b3017faee7c3e12f3",
+    "url": "/static/media/sign-in.6e0a54d1.png"
+  },
+  {
+    "revision": "08e9b7f632d8e8fa9b8f0f9491163597",
+    "url": "/static/media/view.08e9b7f6.png"
+  }
+]);
